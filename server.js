2f440ecc6187e21aa262dd39394d4e3ee7690583
jtdoaks <136029371+jtdoaks@users.noreply.github.com>
2023-08-21 20:34:17 -0400

    added paths to index and notes files. including a path back to the homepage.

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ app.use(express.static("public"));
 app.get("/", (req, res)=> {
     res.sendFile(path.join(__dirname, "/public/index.html"))
 });
+app.get("*", (req, res)=> {
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+});
+app.get("/", (req, res)=> {
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
+});
 
 
 
