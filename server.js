edd31a838673a4fbe3982c05c99715f7b5ef5407
jtdoaks <136029371+jtdoaks@users.noreply.github.com>
2023-08-22 18:29:01 -0400

    constructed uuid for random id generation of each note. created post for user notes. write to file constructed. responses for user and terminal

--- server.js
+++ server.js
@@ -2,23 +2,56 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const app = express();
+const uuid = require('./uuid');
+
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 
-app.get("/", (req, res)=> {
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"))
 });
-app.get("*", (req, res)=> {
+app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"))
 });
-app.get("/", (req, res)=> {
+app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"))
 });
 
+app.post("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
+    console.info(`${req.method} request was recieved to create a note.`);
+
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            post_id: uuid(),
+        };
+        notes.push(newNote)
+        const noteString = JSon.stringify(notes, null, 2);
+
+        fs.writeFile(`./db.json`, noteString, (err) =>
+            err
+                ? console.error(err)
+                : console.log(`Note for ${newNote.title} has been written to db.json file`)
+        );
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        console.log(response)
+        res.status(201).json(response);
+    } else {
+        res.status(500).json('Error is creating note.');
+    };
+});
 
 
 
@@ -26,5 +59,5 @@ app.get("/", (req, res)=> {
 
 
 app.listen(PORT, () =>
-  console.log(`Now listening at http://localhost:${PORT}`)
+    console.log(`Now listening at http://localhost:${PORT}`)
 );
